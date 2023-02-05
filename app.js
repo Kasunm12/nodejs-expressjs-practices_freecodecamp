1b100545cb849f76ac57a1aa913dcb2659d43e4b
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2023-02-06 03:08:02 +0530

    package installation and setup environment

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ const _= require('lodash');
 const items = [1,[2,[3,[4]]]]
 const newItems =_.flattenDeep(items)
 console.log(newItems);
+console.log('kasun weerasekara');
 
 // const first = readFileSync('./content/first.txt','utf8')
 // const second = readFileSync('./content/second.txt','utf8')
